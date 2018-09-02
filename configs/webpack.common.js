ab52934f72d81460f5eb42c845090bcb76acecc0
GuoFei <guofei0723@gmail.com>
2018-09-03 00:00:48 +0800

    build: support decorators

--- configs/webpack.common.js
+++ configs/webpack.common.js
@@ -48,7 +48,10 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['transform-class-properties']
+            plugins: [
+              'transform-class-properties',
+              ['@babel/plugin-proposal-decorators', { legacy: true }],
+            ]
           }
         }
       },
