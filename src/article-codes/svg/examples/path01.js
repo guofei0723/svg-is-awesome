de559e242170118758eb66805f3fdc94f419cda9
GuoFei <guofei0723@gmail.com>
2018-10-21 09:43:34 +0800

    feat(svg): add plain anim to path01

--- src/article-codes/svg/examples/path01.js
+++ src/article-codes/svg/examples/path01.js
@@ -3,12 +3,18 @@ import withDrawPanel from './withDrawPanel';
 
 class _Path01 extends Component {
   $path = React.createRef()
+  $plain = React.createRef()
 
   state = {
     pathLength: 0,
-    pathDashOffset: 0
+    pathDashOffset: 0,
+    // 飞机走过的距离
+    plainDistance: 0,
   }
 
+  /**
+   * 描线动画
+   */
   dashAnim = () => {
     if (!this._prevTime) this._prevTime = new Date()
     let now = new Date()
@@ -27,19 +33,71 @@ class _Path01 extends Component {
     this._animFrame = requestAnimationFrame(this.dashAnim)
   }
 
+  startDashAnim () {
+    this.setState({ pathDashOffset: this.state.pathLength }, this.dashAnim)
+  }
+
+  
+  /**
+   * 沿路径飞行动画
+   */
+  plainAnim = () => {
+    // 初始化上一次时间
+    if (!this._prevPlainTime) this._prevPlainTime = new Date()
+    // 当前时间
+    let now = new Date()
+    // 时间差，秒
+    let diff = (now - this._prevPlainTime) / 1000
+    // 记录本次动画时间
+    this._prevPlainTime = now
+    
+    this.setState(prev => {
+      let distance = prev.plainDistance + diff * 400
+      if (distance >= prev.pathLength) distance -= prev.pathLength
+      return {
+        plainDistance: distance
+      }
+    })
+    
+    this._plainAnimFrame = requestAnimationFrame(this.plainAnim)
+  }
+  
+  /**
+   * 计算飞机transform
+   */
+  calPlainTransform () {
+    if (!this.$path.current) return 
+
+    let { plainDistance } = this.state
+    // 当前距离所在的坐标
+    let {x, y} = this.$path.current.getPointAtLength(plainDistance)
+    // 往后一个单位所在的坐标，用于计算角度
+    let {x: xback, y: yback} = this.$path.current.getPointAtLength(plainDistance - 1)
+    // 方向角度，度数
+    let direction = Math.atan2(y - yback, x - xback) * 180 / Math.PI
+
+    return `translate(${x}, ${y}) rotate(${direction})`
+  }
+
+  startPlainAnim () {
+    this.setState({ plainDistance: 0 }, this.plainAnim)
+  }
+
+
   componentDidMount () {
     let tl = this.$path.current.getTotalLength()
     this.setState({
-      pathLength: tl,
-      pathDashOffset: tl
-    }, this.dashAnim)
+      pathLength: tl
+    }, this.plainAnim)
   }
 
   componentWillUnmount () {
-
+    cancelAnimationFrame(this._animFrame)
+    cancelAnimationFrame(this._plainAnimFrame)
   }
 
   render () {
+    
     return (
       <g 
         transform="translate(430, 155)"
@@ -52,6 +110,12 @@ class _Path01 extends Component {
           strokeDasharray={this.state.pathLength}
           strokeDashoffset={this.state.pathDashOffset}
           ref={this.$path}
+        />
+        <path 
+          d="M -5 0 L -20 -10 L20 0 L-20 10 Z"
+          fill="red"
+          transform={this.calPlainTransform()}
+          ref={this.$plain}
         />
       </g>
     )
